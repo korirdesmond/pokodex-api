f3aa4fd21b15e1b8f9541bcd24746425983d270e
desshub <desshub95@gmail.com>
2022-01-09 00:48:43 +0300

    Added user validations

--- src/services/users/users.hooks.ts
+++ src/services/users/users.hooks.ts
@@ -1,23 +1,43 @@
 import * as feathersAuthentication from '@feathersjs/authentication';
 import * as local from '@feathersjs/authentication-local';
+import { HookContext } from '@feathersjs/feathers';
+import { BadRequest } from '@feathersjs/errors';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = feathersAuthentication.hooks;
 const { hashPassword, protect } = local.hooks;
+
+const validateUserData = async (
+  context: HookContext,
+): Promise<HookContext> => {
+  const { data } = context;
+  const passwordRegex = /^(?!.*\s)(?=.*[a-zA-Z])(?=.*[~`!@#$%^&*()--+={}\[\]|\\:;"'<>,.?/_₹]).{14,}$/;
+  const usernameReqex = /^(?!.*\s)(?=.*[a-zA-Z])(?=.*[-._]).{1,8}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!data.email) throw new BadRequest('Email is required');
+  if (!emailRegex.test(data.email)) throw new BadRequest('Email is not valid');
+  if (!data.username) throw new BadRequest('username is required');
+  if (!usernameReqex.test(data.username) ) throw new BadRequest('username should be between 1 - 8 characters and only allows latin letters, digits and special characters (-, ., _)');
+  if (!data.password) throw new BadRequest('password is required');
+  if (!passwordRegex.test(data.password)) throw new BadRequest('password should contain at least 8 characters and at least on special characters');
+
+  return context;
+};
 
 export default {
   before: {
     all: [],
     find: [ authenticate('jwt') ],
     get: [ authenticate('jwt') ],
-    create: [ hashPassword('password') ],
+    create: [validateUserData, hashPassword('password') ],
     update: [ hashPassword('password'),  authenticate('jwt') ],
     patch: [ hashPassword('password'),  authenticate('jwt') ],
     remove: [ authenticate('jwt') ]
   },
 
   after: {
-    all: [ 
+    all: [
       // Make sure the password field is never sent to the client
       // Always must be the last hook
       protect('password')
